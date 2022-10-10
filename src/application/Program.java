a6e08b536ad766e798e8a6f2d79266cb47a44bcb
Emerson Veloso <emesonveloso.s@gmail.com>
2022-10-10 16:00:43 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,10 @@
 package application;
 
-import bordergame.Position;
+import bordergame.Board;
 
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }
